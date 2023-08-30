44b72870f7a9fd962075f7ddea6482bb86375dc5
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-30 09:00:08 -0500

    Update to for Output Mgmt

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/template.html',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
